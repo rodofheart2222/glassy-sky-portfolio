fd980a69fc1328c5a554b78fee30d6f031c0bfb2
rodofheart2222 <rodofheart2222@gmail.com>
2025-08-16 04:00:56 -0700

    Add basic ping pong game page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Documentation from "./pages/Documentation";
+import PingPong from "./pages/PingPong";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/docs" element={<Documentation />} />
+          <Route path="/ping-pong" element={<PingPong />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
